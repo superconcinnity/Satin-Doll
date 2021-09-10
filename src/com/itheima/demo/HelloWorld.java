9ba3cd9778b1f598346c142c83b74773e1c7dc21
superconcinnity <chengwei2061@gamil.com>
2021-09-10 13:07:47 +0800

    第一次提交

---
+++ src/com/itheima/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima.demo;
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello My Luv 😔");
+
+        // TODO 在这里打字
+        System.out.println("I love Rui");
+
+    }
+}
